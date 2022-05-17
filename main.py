69ba1952f04f9b2a8d0a86d2e6bb19f7fbc998ef
Daniel Torre <daniel.torre.miguel@gmail.com>
2022-05-17 17:40:55 +0200

    Initial version

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ class Plugin:
     # A normal method. It can be called from JavaScript using call_plugin_function("method_2", argument1, argument2)
     async def method_2(self, *args):
         pass
-
+    async def mount():
+        import os
+        os.system("mount -a")
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
         pass
